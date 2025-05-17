866b69beeb190d0013454248967ad89d8320393e
Kamila Musialkowska <cool.it.development@gmail.com>
2025-05-17 19:28:25 +0200

    New Rocket initial status set to ON_GROUND

--- src/main/java/com/kmu/dataobject/Rocket.java
+++ src/main/java/com/kmu/dataobject/Rocket.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 public class Rocket {
 
-    private String name;
+    private final String name;
     private RocketStatus status;
 
     public Rocket(String name) {
@@ -16,16 +16,8 @@ public class Rocket {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public RocketStatus getStatus() {
         return status;
-    }
-
-    public void setStatus(RocketStatus status) {
-        this.status = status;
     }
 
     @Override
